48ba530914027c97e8f9efa535b05371e5c71bd1
Mark90 <mark_moes@live.nl>
2020-12-10 20:58:19 +0100

    day10 cleaner

--- day10/solution.py
+++ day10/solution.py
@@ -10,97 +10,41 @@ def part1(lines, full):
     return d[1] * d[3]
 
 
-def trav(a, i=0):
-    r = 0
-    # while i < len(a):
-    if i + 1 == len(a):
+# Cumulative numbers used in calc_permutations()
+powers_combined = {0: 0, 1: 0, 2: 0}
+for i in range(3, 100):
+    powers_combined[i] = 2 ** (i - 3) + powers_combined[i - 1]
+
+
+def calc_permutations(successive_numbers):
+    # Return the possible adapter permutations for successive numbers.
+    # For 1 (pair of) successive numbers, return 1
+    # for 2 pair, return 2
+    # 3 -> 4     2**(3-1)                 ->  2**2      ->  4
+    # 4 -> 7     2**(4-1)-2**0            ->  8 - 1     ->  7
+    # 5 -> 13    2**(5-1)-2**0-2**1       ->  16-1-2    ->  13
+    # 6 -> 25    2**(6-1)-2**0-2**1-2**2  ->  32-1-2-4  ->  25
+    if successive_numbers == 0:
         return 1
-    if i < len(a) + 1 and a[i] + 3 == a[i + 1]:
-        return 1 + trav(a, i + 1)
-
-    return 1 + r
-
-
-d = {}
-for i in range(25):
-    if i < 3:
-        v = 0
-    else:
-        v = 2 ** (i - 3)
-    d[i] = v + (0 if i == 0 else d[i - 1])
-
-
-def gap1(size):
-    if size == 0:
-        return 1
-    return 2 ** (size - 1) - d[size - 1]
+    return 2 ** (successive_numbers - 1) - powers_combined[successive_numbers - 1]
 
 
 def part2(lines, full):
-    adapters = sorted(list(map(int, lines)))
-    adapters = (
-        [
-            0,
-        ]
-        + adapters
-        + [max(adapters) + 3]
-    )
-    # print(adapters)
-    r = 1
-    cursize = 0
+    # 193434623148032
+    adapters = [0] + sorted(list(map(int, lines)))
+
+    permutations = 1
+    successive_numbers = 0
     i = 0
-    a = adapters
-    while True:
-        if i + 1 == len(a):
-            r *= gap1(cursize)
-            break
-        cur, nex = a[i], a[i + 1]
-        if nex - cur == 3:
-            r *= gap1(cursize)
-            cursize = 0
+    while i + 1 < len(adapters):
+        if adapters[i + 1] - adapters[i] == 3:
+            # Difference with next adapter is 3, cannot skip this one.
+            permutations *= calc_permutations(successive_numbers)
+            successive_numbers = 0
         else:
-            cursize += 1
+            # Difference with next adapter is 1, this one could be skipped
+            # if there are enough successive numbers.
+            successive_numbers += 1
         i += 1
-    return r
-    # r = 0
-    # i = -1
-    # mult = {}  #
-    # la = len(adapters)
-    # a=adapters
-    # from collections import defaultdict
-    # p = defaultdict(dict)
-    # while i+1 < la:
-    #     i += 1
-
-    #     print(f'\n{i=} {a[i]=}')
-    #     if i+1<la and a[i] == a[i+1] + 3:
-    #         # print(f'{i=} {a[i]=} {a[i+1]=}')
-    #         continue
-    #     print(a[i:i+4])
-    #     if i+3 < la and a[i] +3== a[i+3]:
-    #         # print('i',i,'mult',3)
-    #         mult[i] = 4
-    #         # p[i][i+3] = 3
-    #     elif i+2 < la and (a[i+2] - 3 == a[i]  or a[i] == a[i+2] - 2):
-    #         # print('i',i,'mult',2)
-
-    #         # p[i][i+2] = 2
-    #         mult[i] = 2
-    #     # else:
-    #         # print('nop')
-    # print(mult)
-    # r = 1
-    # for k,m in mult.items():
-    #     r*=m
-    # # r = 1
-    # # i = len(a)
-    # # while i > -1:
-    # #     i -= 1
-    # #     if i in p:
-
-    # # for k, v in p.items():
-
-    # print("rrrrr",r)
-    # return r
-    r = trav(adapters)
-    return r
+    permutations *= calc_permutations(successive_numbers)
+    return permutations
