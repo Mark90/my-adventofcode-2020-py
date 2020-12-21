cd5945a955a597ae9fbdd8e3903ecc26f8ff1bd3
Mark90 <mark_moes@live.nl>
2020-12-21 14:09:29 +0100

    day20: r/therewasanattempt

---
+++ day20/__main__.py
@@ -0,0 +1,12 @@
+from pathlib import Path
+
+from .solution import part1, part2
+
+with (Path(__file__).parent / "input.txt").open() as f:
+    _full = f.read()
+
+_lines = _full.splitlines()
+if p1 := part1(_lines, _full):
+    print(f"[part1] Result: {p1}")
+if p2 := part2(_lines, _full):
+    print(f"[part2] Result: {p2}")
